5d1228b560d9e40dcd71e467aeb53e9d4dc91754
Alejandro Valle Torres <alejandrovaltor.x2@gmail.com>
2022-02-25 18:06:09 +0100

    Added put function to updateState

--- OrderApi/Controllers/OrdersController.cs
+++ OrderApi/Controllers/OrdersController.cs
@@ -78,5 +78,27 @@ namespace OrderApi.Controllers
             return NoContent();
         }
 
+        // PUT orders/5
+        [HttpPut("{id}")]
+        public IActionResult PutState(int id, [FromBody] Order order)
+        {
+            if (order == null || order.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var modifiedOrder = repository.Get(id);
+
+            if (modifiedOrder == null)
+            {
+                return NotFound();
+            }
+
+            modifiedOrder.State = order.State;
+
+            repository.Edit(modifiedOrder);
+            return new NoContentResult();
+        }
+
     }
 }
