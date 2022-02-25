b1dff2fd7a7fabbd41f99c1802f20a310089822f
EASV-Pablo <pabl0311@easv365.dk>
2022-02-25 18:23:58 +0100

    CustomerApi_1

---
+++ CustomerApi/Controllers/CustomerContoller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using CustomerApi.Data;
+using CustomerApi.Models;
+using RestSharp;
+
+namespace CustomerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerContoller : ControllerBase
+    {
+        private readonly IRepository<Customer> repository;
+
+        public CustomerContoller(IRepository<Customer> repos)
+        {
+            repository = repos;
+        }
+
+        // GET: customers
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return repository.GetAll();
+        }
+
+        // GET customers/5
+        [HttpGet("{id}", Name="GetCustomer")]
+        public IActionResult Get(int id)
+        {
+            var item = repository.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // POST customer
+        [HttpPost]
+        public IActionResult Post([FromBody]Customer customer)
+        {
+            if (customer == null)
+            {
+                return BadRequest();
+            }
+
+            var newCustomer = repository.Add(customer);
+           
+            return CreatedAtRoute("GetCustomer", new { id = newCustomer.Id }, newCustomer );
+        }
+
+        //CREAR API PUT PARA CAMBIAR DATOS DEL CLIENTE
+        //PUEDE CAMBIAR EMAIL, PHONE, BILLING ADDRESS & SHIPPING ADDRESS
+        
+
+    }
+}
---
+++ CustomerApi/Data/DbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CustomerApi.Models;
+using System;
+
+namespace CustomerApi.Data
+{
+    public class DbInitializer : IDbInitializer
+    {
+        // This method will create and seed the database.
+        public void Initialize(CustomerApiContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            // Look for any Products
+            if (context.Customers.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Id = 1,
+                               Name = "Pedro",
+                               Email = "pedro@gmail.com",
+                               Phone = "+4500000001",
+                               BillingAddress = "Fake street 123, 6500 Esbjerg",
+                               ShippingAddress = "Fake street 123, 6500 Esbjerg",
+                               CreditStanding = 0 },
+
+                new Customer { Id = 2,
+                                Name = "Paco",
+                                Email = "paco@gmail.com",
+                                Phone = "+4500000002",
+                                BillingAddress = "Pidgeon street 1, 6500 Esbjerg",
+                                ShippingAddress = "Pidgeon street 1, 6500 Esbjerg",
+                                CreditStanding = 0 }
+            };
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+        }
+    }
+}
