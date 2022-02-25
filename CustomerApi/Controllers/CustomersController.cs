9a941b904892254042410b40a98c521568f2f736
EASV-Pablo <pabl0311@easv365.dk>
2022-02-25 20:38:42 +0100

    Puto CutomerSSSSSSController

--- CustomerApi/Controllers/CustomersController.cs
+++ CustomerApi/Controllers/CustomersController.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using CustomerApi.Data;
 using CustomerApi.Models;
-using RestSharp;
 
 namespace CustomerApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CustomerContoller : ControllerBase
+    public class CustomersController : ControllerBase
     {
         private readonly IRepository<Customer> repository;
 
-        public CustomerContoller(IRepository<Customer> repos)
+        public CustomersController(IRepository<Customer> repos)
         {
             repository = repos;
         }
@@ -26,7 +25,7 @@ namespace CustomerApi.Controllers
         }
 
         // GET customers/5
-        [HttpGet("{id}", Name="GetCustomer")]
+        [HttpGet("{id}", Name = "GetCustomer")]
         public IActionResult Get(int id)
         {
             var item = repository.Get(id);
@@ -37,23 +36,9 @@ namespace CustomerApi.Controllers
             return new ObjectResult(item);
         }
 
-        // POST customer
+        // POST customers
         [HttpPost]
-        public IActionResult Post([FromBody]Customer customer)
-        {
-            if (customer == null)
-            {
-                return BadRequest();
-            }
-
-            var newCustomer = repository.Add(customer);
-           
-            return CreatedAtRoute("GetCustomer", new { id = newCustomer.Id }, newCustomer );
-        }
-
-        // PUT customer
-        [HttpPut]
-        public IActionResult Put([FromBody]Customer customer)
+        public IActionResult Post([FromBody] Customer customer)
         {
             if (customer == null)
             {
@@ -65,9 +50,30 @@ namespace CustomerApi.Controllers
             return CreatedAtRoute("GetCustomer", new { id = newCustomer.Id }, newCustomer);
         }
 
-        //CREAR API PUT PARA CAMBIAR DATOS DEL CLIENTE
-        //PUEDE CAMBIAR EMAIL, PHONE, BILLING ADDRESS & SHIPPING ADDRESS
+        // PUT customers/3
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Customer customer)
+        {
+            if (customer == null || customer.Id != id)
+            {
+                return BadRequest();
+            }
 
+            var modifiedCustomer = repository.Get(id);
+
+            if (modifiedCustomer == null)
+            {
+                return NotFound();
+            }
+
+            modifiedCustomer.Phone = customer.Phone;
+            modifiedCustomer.ShippingAddress = customer.ShippingAddress;
+            modifiedCustomer.BillingAddress = customer.BillingAddress;
+            modifiedCustomer.Email = customer.Email;
+
+            repository.Edit(modifiedCustomer);
+            return new NoContentResult();
+        }
 
 
     }
