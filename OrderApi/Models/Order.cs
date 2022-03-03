ba112d0f8ebf2153c2ef3a4358622d2ede38aae3
Alejandro Valle Torres <alejandrovaltor.x2@gmail.com>
2022-03-03 15:27:01 +0100

    Creada nueva clase y cambios en el inicializador de la base de datos

--- OrderApi/Models/Order.cs
+++ OrderApi/Models/Order.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace OrderApi.Models
 {
     public class Order
@@ -7,8 +10,8 @@ namespace OrderApi.Models
         public DateTime? Date { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
+        public List<ProductQuantity> ProductList { get; set; } = new List<ProductQuantity>();
         public int CustomerId { get; set; }
-
         public Status State { get; set; }
 
         public enum Status
---
+++ OrderApi/Models/ProductQuantity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+namespace OrderApi.Models
+{
+    [Keyless]
+    public class ProductQuantity
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
