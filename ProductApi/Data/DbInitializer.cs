e84278e83fcf1e2cc0c8bb3211a18b5048558b46
EASV-Pablo <pabl0311@easv365.dk>
2022-03-03 10:56:32 +0100

    Se ha incluido el atributo "Category" al modelo products y se han modificado todos los métodos que lo podían usar.

--- ProductApi/Data/DbInitializer.cs
+++ ProductApi/Data/DbInitializer.cs
@@ -20,9 +20,9 @@ namespace ProductApi.Data
 
             List<Product> products = new List<Product>
             {
-                new Product { Name = "Hammer", Price = 100, ItemsInStock = 10, ItemsReserved = 2 },
-                new Product { Name = "Screwdriver", Price = 70, ItemsInStock = 20, ItemsReserved = 0 },
-                new Product { Name = "Drill", Price = 500, ItemsInStock = 2, ItemsReserved = 0 }
+                new Product { Name = "Hammer", Price = 100, Category = "Tools", ItemsInStock = 10, ItemsReserved = 2 },
+                new Product { Name = "Screwdriver", Price = 70, Category = "Tools", ItemsInStock = 20, ItemsReserved = 0 },
+                new Product { Name = "Drill", Price = 500, Category = "Tools", ItemsInStock = 2, ItemsReserved = 0 }
             };
 
             context.Products.AddRange(products);
