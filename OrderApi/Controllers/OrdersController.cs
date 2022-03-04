b674b918be51a081f809378245c73f5adf68be08
EASV-Pablo <pabl0311@easv365.dk>
2022-03-04 13:54:47 +0100

    Endpoint CancelOrder has been updated to cancel orders with multiple products.

--- OrderApi/Controllers/OrdersController.cs
+++ OrderApi/Controllers/OrdersController.cs
@@ -157,22 +157,20 @@ namespace OrderApi.Controllers
 
             if (order.State == Order.Status.Completed)
             {
-                RestClient c = new RestClient("https://localhost:5001/products/");
-                var request = new RestRequest(order.ProductId.ToString());
-                var response = c.GetAsync<Product>(request);
-                response.Wait();
-                var product = response.Result;
+                bool productsUpdated= cancelProductStock(order);
 
-                product.ItemsReserved -= order.Quantity;
-
-                var updateRequest = new RestRequest(product.Id.ToString());
-                updateRequest.AddJsonBody(product);
-                var updateResponse = c.PutAsync(updateRequest);
-                updateResponse.Wait();
-                if (updateResponse.IsCompletedSuccessfully)
+                if (productsUpdated)
                 {
                     order.State = Order.Status.Cancelled;
                     repositoryOrders.Edit(order);
+
+                    Customer customer = getCustomer(order.CustomerId);
+                    customer.CreditStanding -= totalAmount(order);
+                    setCustomer(customer);
+
+                    string content = String.Format("The order with id: {0}, has been cancelled.", order.Id);
+                    sendEmail(customer.Email, content);
+
                     return new NoContentResult();
                 }
                 else
@@ -194,7 +192,7 @@ namespace OrderApi.Controllers
 
             if (order.State == Order.Status.Completed)
             {
-                bool productsUpdated = decreaseProductStock(order);
+                bool productsUpdated = sendProductStock(order);
 
                 if (productsUpdated)
                 {
@@ -292,7 +290,7 @@ namespace OrderApi.Controllers
             return orderedCustomer;
         }
     
-        private bool decreaseProductStock(Order order)
+        private bool sendProductStock(Order order)
         {
             bool aux = true;
             foreach(OrderLine line in order.orderLines)
@@ -305,6 +303,31 @@ namespace OrderApi.Controllers
                 aux = setProduct(product);
             }
             return aux;
+        }
+
+        private bool cancelProductStock(Order order)
+        {
+            bool aux = true;
+            foreach (OrderLine line in order.orderLines)
+            {
+                Product product = getProduct(line.ProductId);
+
+                product.ItemsReserved -= line.Quantity;
+
+                aux = setProduct(product);
+            }
+            return aux;
+        }
+
+        private int totalAmount(Order order)
+        {
+            int total = 0;
+            foreach (OrderLine line in order.orderLines)
+            {
+                Product product = getProduct(line.ProductId);
+                total += line.Quantity * product.Price;
+            }            
+            return total;
         }
 
 
--- ProductApi/Models/Product.cs
+++ ProductApi/Models/Product.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
         public string Category { get; set; }
         public int ItemsInStock { get; set; }
         public int ItemsReserved { get; set; }
